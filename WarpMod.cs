6b821a1b93338a017ff62ecc308c3ed4bdd6e848
LeeMoriya <leecassie12@gmail.com>
2023-03-15 18:33:56 +0000

    1.82 wip

--- WarpMod.cs
+++ WarpMod.cs
@@ -5,17 +5,38 @@ using System;
 using UnityEngine;
 using System.Reflection;
 using BepInEx;
+using BepInEx.Logging;
 
-[BepInPlugin("LeeMoriya.Warp", "Warp", "1.81")]
+[BepInPlugin("LeeMoriya.Warp", "Warp", "1.82")]
 public class WarpMod : BaseUnityPlugin
 {
     public bool init = false;
+    public ManualLogSource logger;
     public WarpMod()
     {
-
+        logger = new ManualLogSource("Warp");
     }
     public void OnEnable()
     {
+        On.RainWorld.OnModsInit += RainWorld_OnModsInit;
+        On.RainWorld.Update += RainWorld_Update;
+    }
+
+    private void RainWorld_Update(On.RainWorld.orig_Update orig, RainWorld self)
+    {
+        try
+        {
+            orig.Invoke(self);
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+        }
+    }
+
+    private void RainWorld_OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    {
+        orig.Invoke(self);
         if (!init)
         {
             WarpModMenu.MenuHook();
